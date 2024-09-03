21ce5323301130bb72b5bc9f2373fa9541d1de56
Ananya Rao <ananya.rao@gmail.com>
2024-09-03 14:44:45 -0500

    fixing room reservation

--- jones-website/src/components/pages/roomreservation/RoomReservation.js
+++ jones-website/src/components/pages/roomreservation/RoomReservation.js
@@ -1,63 +1,64 @@
-import React, { useState, useRef, useEffect } from 'react';
-import moment from 'moment';
+// import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
+// import moment from 'moment';
 import './RoomReservation.css'; 
-import { BsChevronRight, BsChevronLeft, BsCalendar2Fill } from "react-icons/bs";
-import DatePicker from "react-datepicker";
-import {PostEvent} from './PostEvent';
-import RoomEventView from './RoomEventView';
-import ReservationForm from './ReservationForm';
-import { FetchRooms } from './FetchRooms';
+// import { BsChevronRight, BsChevronLeft, BsCalendar2Fill } from "react-icons/bs";
+// import DatePicker from "react-datepicker";
+// import {PostEvent} from './PostEvent';
+// import RoomEventView from './RoomEventView';
+// import ReservationForm from './ReservationForm';
+// import { FetchRooms } from './FetchRooms';
 import Footer from '../../Footer';
 
 
 function RoomReservation() {
-  const [selectedDay, setSelectedDay] = useState(new Date())
-  const [commonsEvents, setCommonsEvents] = useState([])
-  const [pdrEvents, setPdrEvents] = useState([])
-  const [kitchenEvents, setKitchenEvents] = useState([])
-  const [movieRoomEvents, setMovieRoomEvents] = useState([])
+  // const [selectedDay, setSelectedDay] = useState(new Date())
+  // const [commonsEvents, setCommonsEvents] = useState([])
+  // const [pdrEvents, setPdrEvents] = useState([])
+  // const [kitchenEvents, setKitchenEvents] = useState([])
+  // const [movieRoomEvents, setMovieRoomEvents] = useState([])
 
-  const datePicker = useRef()
+  // const datePicker = useRef()
 
-  useEffect(() => {
-    FetchRooms('Commons', setCommonsEvents)
-    FetchRooms('PDR', setPdrEvents)
-    FetchRooms('Jitchen', setKitchenEvents)
-    FetchRooms('Movie Room', setMovieRoomEvents)
+  // useEffect(() => {
+  //   FetchRooms('Commons', setCommonsEvents)
+  //   FetchRooms('PDR', setPdrEvents)
+  //   FetchRooms('Jitchen', setKitchenEvents)
+  //   FetchRooms('Movie Room', setMovieRoomEvents)
 
-    let url = new URL(window.location.href);
-    const accessTokenFromFragment = url.hash.includes('access_token') ? url.hash.split('access_token=')[1] : '';
-    const accessToken = accessTokenFromFragment.includes('&token_type=') ? accessTokenFromFragment.split('&token_type=')[0] : '';
-    const eventInfo = sessionStorage.getItem('eventInfo');
-    if (accessToken !== "" && eventInfo) {
-      PostEvent(accessToken, eventInfo).then(() => {
-          // just automatically reload page to get new list
-          setTimeout(function() {
-            // Your code to be executed after 1 second
-            window.location.href = "/reserve-room"
-            window.history.pushState({}, "", "/reserve-room")
-        }, 1000);
+  //   let url = new URL(window.location.href);
+  //   const accessTokenFromFragment = url.hash.includes('access_token') ? url.hash.split('access_token=')[1] : '';
+  //   const accessToken = accessTokenFromFragment.includes('&token_type=') ? accessTokenFromFragment.split('&token_type=')[0] : '';
+  //   const eventInfo = sessionStorage.getItem('eventInfo');
+  //   if (accessToken !== "" && eventInfo) {
+  //     PostEvent(accessToken, eventInfo).then(() => {
+  //         // just automatically reload page to get new list
+  //         setTimeout(function() {
+  //           // Your code to be executed after 1 second
+  //           window.location.href = "/reserve-room"
+  //           window.history.pushState({}, "", "/reserve-room")
+  //       }, 1000);
         
-        // if (resp.status == "confirmed") {
-        //   console.log(addedEvent)
-        //   if (addedEvent[0] == "Commons") {
-        //     commonsEvents.push(addedEvent[1])
-        //   }
-        //   else if (addedEvent[0] == "Jitchen") {
-        //     commonsEvents.push(addedEvent[1])
-        //   }
-        //   else if (addedEvent[0] == "PDR") {
-        //     console.log("event pushed")
-        //     commonsEvents.push(addedEvent[1])
-        //   }
-        //   else if (addedEvent[0] == "Movie Room") {
-        //     commonsEvents.push(addedEvent[1])
-        //   }
-        //}
-      })
-      sessionStorage.clear()
-    }
-  }, [])
+  //       // if (resp.status == "confirmed") {
+  //       //   console.log(addedEvent)
+  //       //   if (addedEvent[0] == "Commons") {
+  //       //     commonsEvents.push(addedEvent[1])
+  //       //   }
+  //       //   else if (addedEvent[0] == "Jitchen") {
+  //       //     commonsEvents.push(addedEvent[1])
+  //       //   }
+  //       //   else if (addedEvent[0] == "PDR") {
+  //       //     console.log("event pushed")
+  //       //     commonsEvents.push(addedEvent[1])
+  //       //   }
+  //       //   else if (addedEvent[0] == "Movie Room") {
+  //       //     commonsEvents.push(addedEvent[1])
+  //       //   }
+  //       //}
+  //     })
+  //     sessionStorage.clear()
+  //   }
+  // }, [])
   
   return (
   <div className='room-reservation-container'>
